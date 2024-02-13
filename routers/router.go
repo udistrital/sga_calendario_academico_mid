d1e18d9b56d80ee8a3e353fee31cd6d2ba3018d7
NeFaWaltPint <nefawaltpint@gmail.com>
2024-02-13 10:28:44 -0500

    fix: lineamiento mover _mid al final para vars

--- routers/router.go
+++ routers/router.go
@@ -9,7 +9,7 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/udistrital/sga_mid_calendario_academico/controllers"
+	"github.com/udistrital/sga_calendario_academico_mid/controllers"
 	"github.com/udistrital/utils_oas/errorhandler"
 )
 
